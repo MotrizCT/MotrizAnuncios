361182b462de89d14b93de6fe1cdb959d5f2e736
MotrizCT <105676205+MotrizCT@users.noreply.github.com>
2022-05-17 22:08:01 +0200

    Add files via upload

---
+++ motriz_anuncios/fxmanifest.lua
@@ -0,0 +1,36 @@
+fx_version 'cerulean'
+
+game 'gta5'
+
+author 'Titanes'
+
+description 'Announce System ReBuild'
+
+shared_scripts {
+    '@es_extended/imports.lua'
+}
+
+client_scripts {
+    'Client/Modules/*.lua'
+}
+
+ui_page 'ui/index.html'
+
+files {
+	'ui/index.html',
+	'ui/style.css',
+	'ui/app.js',
+    'ui/bankgothic.ttf',
+    'ui/bootstrap.min.css',
+    'ui/futura.ttf',
+    'ui/chart.js',
+    'ui/circle-progress.js',
+    'ui/tablet_start.ogg',
+    'ui/tablet_app.ogg',
+    'ui/circle-loading.svg',
+    'ui/loading2.svg',
+    'ui/anuncio.ogg',
+    'ui/codigo-penal.html',
+    'ui/loading-white.svg',
+
+}
